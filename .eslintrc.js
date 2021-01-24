0089a7d3dd0fc96fb512aad9f72bd5078664e657
YouSay-SD <yousay.sd@gmail.com>
2021-01-23 21:35:45 -0300

    build: setup project

---
+++ .eslintrc.js
@@ -0,0 +1,70 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'prettier',
+  ],
+  rules: {
+    'no-console': 0,
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+    ],
+    'no-var': 'error',
+    'brace-style': 'error',
+    'prefer-template': 'error',
+    radix: 'error',
+    'space-before-blocks': 'error',
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': 'off',
+    'jsx-quotes': [2, 'prefer-single'],
+    'react/jsx-filename-extension': [0],
+    'import/extensions': 'off',
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: 'always',
+        ObjectPattern: {
+          multiline: true,
+        },
+        ImportDeclaration: 'never',
+        ExportDeclaration: {
+          multiline: true,
+          minProperties: 3,
+        },
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+        ],
+      },
+    },
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  endOfLine: 'crlf',
+  trailingComma: 'es5',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 50,
+}
