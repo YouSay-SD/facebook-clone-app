b1ed94023b1a78dbe7c3a5ec830414c814923108
YouSay-SD <yousay.sd@gmail.com>
2021-02-13 20:09:12 -0300

    build: npm install react-hook-form

--- src/components/auth/FormLogin/index.tsx
+++ src/components/auth/FormLogin/index.tsx
@@ -1,126 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import validator from 'validator';
+import { useForm } from 'react-hook-form';
 import { Form, Button, Input, Archor } from './styles';
-import { useForm } from '../../../hooks/useForm';
 import { FormData, FormElement } from './interface';
 
 const FormLogin: React.FC = () => {
-  const { formValues, handleInputChange } = useForm<FormData>({
-    email: '',
-    password: '',
-  });
+  const { register, errors, handleSubmit } = useForm<FormData>();
 
-  const { email, password } = formValues;
-
-  const [valid, setValid] = useState({
-    isEmailValid: true,
-    // isPasswordValid: true,
-  });
-
-  useEffect(() => {
-    setValid({
-      ...valid,
-      isEmailValid: email.length > 1,
-    });
-  }, [setValid]);
-
-  // const { isEmailValid, isPasswordValid, isAllValid } = valid;
-
-  // const isFormValid = () => {
-  //   console.log(validator.isEmail(email));
-  //   console.log(password.trim().length > 5);
-  //   console.log(validator.isEmail(email) && password.trim().length > 5);
-
-  //   setValid({
-  //     ...valid,
-  //     isEmailValid: validator.isEmail(email),
-  //     isPasswordValid: password.trim().length > 5,
-  //     isAllValid: validator.isEmail(email) && password.trim().length > 5,
-  //   });
-
-  //   // console.log(valid);
-  //   // return isAllValid;
-  // };
-
-  const handleSubmit = (e: FormElement) => {
-    e.preventDefault();
-
-    // setValid({
-    //   ...valid,
-    //   isEmailValid: validator.isEmail(email),
-    //   isPasswordValid: password.trim().length > 5,
-    //   isAllValid: validator.isEmail(email) && password.trim().length > 5,
-    // });
-
-    // const isEmail = new Promise((resolve, reject) => {
-    //   if (validator.isEmail(email)) {
-    //     resolve(true);
-    //   } else {
-    //     // eslint-disable-next-line prefer-promise-reject-errors
-    //     reject(false);
-    //   }
-    // });
-
-    // isEmail.then((res) => {
-    //   setValid({
-    //     ...valid,
-    //     isEmailValid: res,
-    //     // isPasswordValid: password.trim().length > 5,
-    //   });
-    // });
-
-    console.log('console', email.length > 1);
-    // const isE = email.length > 1;
-    setValid({
-      ...valid,
-      isEmailValid: validator.isEmail(email),
-      // isPasswordValid: password.trim().length > 5,
-    });
-
-    // console.log(valid);
-    // if (isFormValid()) {
-    //   console.log('Form Correcto');
-    // }
+  const onSubmit = ({ email, password }: FormData) => {
+    console.log(email, password);
   };
-  // console.log(valid);
-  console.log('renderizo');
-
-  // const test = () => {
-  //   console.log(email);
-  //   setValid({
-  //     ...valid,
-  //     isEmailValid: email.length > 1,
-  //     // isPasswordValid: password.trim().length > 5,
-  //   });
-  // };
-
-  // useEffect(() => {
-  //   test();
-  // }, []);
-
-  // console.log(valid);
 
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit(onSubmit)}>
       <Input
         type='email'
         placeholder='Email or Phone Number'
         name='email'
-        value={email}
-        onChange={handleInputChange}
-        // validate='false'
+        ref={register({ required: true })}
       />
-      {/* {!isEmailValid && 'Error Email'} */}
+      {errors.email && 'Enter your email'}
       <Input
         type='password'
         placeholder='Password'
         name='password'
-        value={password}
-        onChange={handleInputChange}
+        ref={register({ required: true })}
         autoComplete='false'
       />
-      {/* {!isPasswordValid && 'Error Password'} */}
-
+      {errors.email && 'Enter your password'}
       <Button type='submit' value='Log In' />
       <Archor>
         <a href='/'>Forgot Password?</a>
