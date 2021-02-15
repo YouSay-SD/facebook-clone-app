78e587af824cae12bf5a0016e99cad3d0a45a4c4
YouSay-SD <yousay.sd@gmail.com>
2021-02-14 23:55:30 -0300

    style: added styles for Hero component

--- src/components/profile/Hero/index.tsx
+++ src/components/profile/Hero/index.tsx
@@ -1,12 +1,31 @@
 import React, { FC } from 'react';
-import { Container } from '../..';
-import { Image } from './styles';
+import { Container, P } from '../..';
+import {
+  HeroContainer,
+  Image,
+  Avatar,
+  BlurImage,
+  TextContainer,
+  UserName,
+} from './styles';
 
 const Hero: FC = () => {
   return (
-    <Container hasPaddingMobile={false}>
-      <Image url='https://squadstate.com/wp-content/uploads/2020/12/Sucrose-Genshin-1024x576.png' />
-    </Container>
+    <HeroContainer>
+      <Container hasPaddingMobile={false}>
+        <Image url='https://www.pockettactics.com/wp-content/uploads/2021/01/genshin-impact-ganyu.jpg'>
+          <Avatar url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRRn8HIKq1xFSeHJTiI7EBp1Cs_M0LCDxZgw&usqp=CAU' />
+        </Image>
+        <TextContainer>
+          <UserName>YouSay</UserName>
+          <P>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </P>
+        </TextContainer>
+      </Container>
+      <BlurImage url='https://liukin.es/wp-content/uploads/2020/12/1607717844_163_El-programa-especial-Genshin-Impact-12-mostro-a-Ganyu-Albedo.jpg' />
+    </HeroContainer>
   );
 };
 
--- src/components/profile/Hero/styles.ts
+++ src/components/profile/Hero/styles.ts
@@ -6,8 +6,78 @@ interface ImageProps {
 
 export const Image = styled.div<ImageProps>`
   width: 100%;
-  height: 300px;
+  height: 250px;
   background-image: url(${({ url }) => url || ''});
   background-position: center;
   background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+`;
+
+export const HeroContainer = styled.div`
+  padding-bottom: 20px;
+  position: relative;
+`;
+
+export const BlurImage = styled.div<ImageProps>`
+  background-image: url(${({ url }) => url || ''});
+  background-size: cover;
+  background-position: center;
+  filter: blur(20px);
+  transform: scale(1.2);
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -2;
+
+  &:after {
+    content: '';
+    background: linear-gradient(
+      0deg,
+      rgba(65, 25, 25, 1) 24%,
+      rgba(255, 255, 255, 0) 70%
+    );
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+  }
+`;
+
+export const Avatar = styled.div<ImageProps>`
+  width: 180px;
+  height: 180px;
+  background-image: url(${({ url }) => url || ''});
+  background-position: center;
+  background-size: cover;
+  border-radius: 50%;
+  border: 4px solid ${({ theme }) => theme.colors.primary};
+  margin-bottom: -20px;
+`;
+
+export const UserName = styled.h1`
+  font-size: 1.6rem;
+  color: ${({ theme }) => theme.colors.fontColorPrimary};
+  margin-bottom: 9px;
+  font-weight: 800;
+`;
+
+export const TextContainer = styled.div`
+  padding-top: 30px;
+  max-width: 300px;
+  width: 100%;
+  margin: 0 auto;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+
+  p {
+    opacity: 0.7;
+  }
 `;
