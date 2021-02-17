0f0cdc28e70d6f195aec0c30fee0b7ca4544a9d5
YouSay-SD <yousay.sd@gmail.com>
2021-02-16 22:21:32 -0300

    feat: create PostInfo component

---
+++ src/components/post/PostInfo/index.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import { PostInfoContainer } from './styles';
+
+const PostInfo: FC = ({ children }) => {
+  return <PostInfoContainer />;
+};
+
+export default PostInfo;
