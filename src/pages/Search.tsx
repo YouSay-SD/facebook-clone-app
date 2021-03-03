52a00978cf6c3ce98a7396d1aff9e7ac2c8a4abb
YouSay-SD <yousay.sd@gmail.com>
2021-03-02 23:52:20 -0300

    feat: create Search page

---
+++ src/pages/Search.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { SearchContent } from '../components';
+
+const Search: FC = () => {
+  return (
+    <>
+      <SearchContent />
+    </>
+  );
+};
+
+export default Search;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export { default as Login } from './Login';
 export { default as Profile } from './Profile';
 export { default as Photos } from './Photos';
+export { default as Search } from './Search';
